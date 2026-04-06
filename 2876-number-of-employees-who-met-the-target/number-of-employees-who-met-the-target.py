a473a9912eb531a462fe45826d6f253262cf3da2
Alisha Shaik <phodogaming4@gmail.com>
2026-04-06 23:31:27 +0530

    Time: 1 ms (15.52%) | Memory: 19.5 MB (8.13%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
@@ -1,7 +1,3 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
-        count=0
-        for num in hours:
-            if num>=target:
-                count+=1
-        return count
+        return sum([1 for i in hours if i>=target])
