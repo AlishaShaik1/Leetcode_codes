ba033948371a29760523f9c19c8d4223acf4c36e
Alisha Shaik <phodogaming4@gmail.com>
2026-06-03 22:23:14 +0530

    Time: 4 ms (45.83%) | Memory: 19.3 MB (16.03%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        result,count=1,1
+        for i in range(len(s)-1):
+
+            if s[i]==s[i+1]:
+                count+=1
+                result=max(result,count)
+            else:
+                count=1
+        return result
